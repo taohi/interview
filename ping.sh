26b8151756b5219d0085e8315ba7761406d957fe
huang haitao <tao_08@qq.com>
2013-12-19 16:42:06 +0800

    modified:   ping.sh

--- ping.sh
+++ ping.sh
@@ -2,10 +2,11 @@
 #ping 192.168.0.*~192.168.5.*网段，如果主机是通的，记录主机IP
 LOG_FILE=/home/active_ip.txt
 
-i=1
 date >>"$LOG_FILE"
 for j in {0,1,2,3,4,5}
 do
+    echo "$j"
+    i=1
     while [ "$i" -le 255 ]
     do
         ping -c1 192.168."$j"."$i"|grep ttl|cut -d: -f1|cut -d' ' -f4 >>"$LOG_FILE"
