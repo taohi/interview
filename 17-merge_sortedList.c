0eb0e223b81a99b45cbcf14b1b24f67cd641e117
haitao <tao_08@qq.com>
2014-06-19 14:53:45 +0800

    剑指offer面试题17:合并两个已经排好序的链表

---
+++ 17-merge_sortedList.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct node{
+    int key;
+    struct node *next;
+}ListNode;
+
+ListNode *createList()
+{
+    int key;
+    ListNode *p=NULL;
+    ListNode *pHead=NULL, *tail=NULL;
+    printf("Input sorted list node:");
+    scanf("%d",&key);
+    while(key!=-1)
+    {
+       p=(ListNode *)malloc(sizeof(ListNode));
+       p->key=key;
+       p->next=NULL;
+       if(pHead == NULL)
+       {
+           pHead=p;
+           tail=p;
+       }
+       else
+       {
+            tail->next=p;
+            tail=tail->next;
+       }
+       scanf("%d",&key);
+    }
+    return pHead;
+}
+
+void printList(ListNode *phead)
+{
+    while(phead)
+    {
+        printf("%d\t",phead->key);
+        phead=phead->next;
+    }
+    printf("\n");
+}
+
+ListNode *merge_sortedList(ListNode *phead1,ListNode *phead2)
+{
+    ListNode *mergedHead=NULL;
+    if(phead1==NULL)
+        return phead2;
+    if(phead2==NULL)
+        return phead1;
+    if(phead1->key < phead2->key)
+    {
+        mergedHead = phead1;
+        mergedHead->next = merge_sortedList(phead2,phead1->next);
+    }
+    else
+    {
+        mergedHead = phead2;
+        mergedHead->next = merge_sortedList(phead2->next,phead1);
+    }
+    return mergedHead;
+}
+
+int main()
+{
+    ListNode *pHead1=createList();
+    ListNode *pHead2=createList();
+    printf("After Merge:");
+    printList(merge_sortedList(pHead1,pHead2));
+    return 0;
+}
