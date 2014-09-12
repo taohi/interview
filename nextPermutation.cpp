e47a49401380c7bd006a76ed02a5f93a6ac9cdf6
taohi <taohi@taohi.net>
2014-09-12 16:18:45 +0800

    leetcode题目：next Permutation

---
+++ nextPermutation.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace::std;
+
+void printNum(vector<int> num)
+{
+    for(int i=0;i<num.size();i++)
+        cout<<num[i]<<" ";
+    cout<<endl;
+}
+
+void nextPermutation(vector<int> &num)
+{
+    if(num.size()==0||num.size()==1)
+        return;
+    vector<int>::iterator iter1=num.end()-1;
+    vector<int>::iterator iter2=num.end()-2;
+    while(*iter2>=*iter1)
+    {
+        if(iter2==num.begin())
+            iter2=num.end()-1;
+        else
+            iter2--;
+        if(iter1==num.begin())
+            iter1=num.end()-1;
+        else
+            iter1--;
+    }
+    vector<int>::iterator iter3=num.end()-1;
+    while(*iter2>=*iter3)
+    {
+        if(iter3==num.begin())
+            iter3=num.end()-1;
+        else
+            iter3--;
+    }
+    swap(*iter2,*iter3);
+    reverse(iter1,num.end());
+} 
+int main()
+{
+    vector<int> num;
+    int temp;
+    while(cin>>temp)
+        num.push_back(temp);
+    nextPermutation(num);
+    printNum(num);
+    return 0;
+}
